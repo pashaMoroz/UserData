3025ede1a8437af9d9458091718871871be20dee
Pavel <vferme2015@gmail.com>
2019-07-11 10:06:35 +0300

    Initial Commit

---
+++ UserData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserData
+//
+//  Created by Pasha Moroz on 7/11/19.
+//  Copyright © 2019 Pavel Moroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
